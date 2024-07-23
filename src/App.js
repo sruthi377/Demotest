7113b00295556ce27a394b3a21e0b458a2dfd865
sruthi377 <sruthi@cogniphi.com>
2024-07-23 18:31:08 +0530

    changes commited app js files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     { Name: 'John', Age: 28, City: 'New York' },
     { Name: 'Anna', Age: 22, City: 'London' },
     { Name: 'Mike', Age: 32, City: 'San Francisco' },
+    { Name: 'Mikes', Age: 78, City: 'Sans Francisco' },
   ];
 
   return (
